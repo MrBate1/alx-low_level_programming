6f417785591e2bc5f3b5a6dfb4c9f4047cadb3e8
MrBate1 <joshuabarker18@gmail.com>
2023-03-02 05:38:05 -0800

    changed code completely

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,16 @@
  *Return: dest concatenated strings
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] < n && src[j] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' && src[j] < n; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
-	dest[i] = '\0';
-
 	return (dest);
 }
