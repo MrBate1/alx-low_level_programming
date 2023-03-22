000b0a2b23044889f6ea2b56d033bf634d90def6
MrBate1 <joshuabarker18@gmail.com>
2023-03-22 14:47:57 -0700

    changed main.h to func..h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  *print_name - function that call a function that prints a name
@@ -9,8 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
 	f(name);
 }
