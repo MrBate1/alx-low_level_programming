985babe3a25a8aee6e64973d2f4d5adc6de7c1ff
MrBate1 <joshuabarker18@gmail.com>
2023-03-30 10:45:25 -0700

    fixed betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,20 +3,19 @@
 /**
  * list_len - Finds the number of elements in
  *            a linked list_t list.
- * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
+ * @h: The linked list_t list
  *
  * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t el = 0;
 
 	while (h)
 	{
-		elements++;
+		el++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (el);
 }
